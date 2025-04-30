cce4daf83781a1476e6d6cb086f3a13b0e210e53
AdsonVicente707 <adsonvicente332211@gmail.com>
2025-04-30 08:46:15 -0300

    JS

---
+++ aula11/exer008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { 
+    console.log(`você ultrapassou a velocidade permitida de 60km/h`) 
+}
+console.log(`Dirija com cuidado!`)
---
+++ aula11/exer009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log (`vivendo no ${pais}`)
+if (pais == 'Brasil') {
+    console.log (`você é brasileiro`)
+}
+else {
+    console.log (`você é estrangeiro`)
+}
